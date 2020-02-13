71d584ddb3daa546d576ad62e23b9fe9430f395e
Grimsever <fupkaa@gmail.com>
2020-02-13 08:35:01 +0300

    Add all files

---
+++ FileStorage/FileStorage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+
+namespace FileStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //todo переедет в метод, какого-нибудь класса, мб буду юзать DI контейнер, но это не точно
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfiguration configuration = builder.Build();
+            var appConfigValue = configuration.GetSection("user").GetChildren().ToArray();
+
+            User user = new User(appConfigValue[0].Value, appConfigValue[1].Value);
+
+            Console.WriteLine("Write login");
+            string login = Console.ReadLine();
+            Console.WriteLine("Write password");
+            string password = Console.ReadLine();
+            Console.WriteLine(user.LogIn(login,password)? "Ow, login successful":"Your data is`t correct");
+        }
+    }
+}
+
---
+++ FileStorage/FileStorage/User.cs
@@ -0,0 +1,33 @@
+﻿namespace FileStorage
+{
+    public class User
+    {
+        private string Login { get; set; }
+
+        private string Password { get; set; }
+
+        public bool IsAuthenticated { get; private set; }
+
+        public User(string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+        
+        public bool LogIn(string login, string password)
+        {
+            if (Login == login && Password == password)
+            {
+                IsAuthenticated = true;
+                return IsAuthenticated;
+            }
+
+            return IsAuthenticated;
+        }
+
+        public override string ToString()
+        {
+            return $"Ow, login successful your data login is {Login} password is {Password}";
+        }
+    }
+}
